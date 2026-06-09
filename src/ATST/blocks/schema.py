c19ffd12c11f3ffa04fe5041cf776d14d8464d20
jpmanti <jpmanti@gmail.com>
2026-06-09 18:00:37 +0800

    updated import

--- src/ATST/blocks/schema.py
+++ src/ATST/blocks/schema.py
@@ -2,7 +2,7 @@
 
 from dataclasses import dataclass
 
-from ATST.blocks import LAYOUT_SCHEMA
+from ATST.blocks.block_names import LAYOUT_SCHEMA
 from ATST.blocks.base_classes import LongTableBlock, WideTableBlock
 
 
